a6db369ebd3a9fa37b8025e8f5bdf35fa2992a94
adrianl01 <gustavo.adrian.leiva879@gmail.com>
2024-08-23 11:56:52 -0300

    second commit

---
+++ lib/requests.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+
+export function getOffsetAndLimitFromReq(req: NextApiRequest, maxLimit, maxOffset) {
+    const queryLimit = parseInt(req.query.limit as string)
+    const queryOffset = parseInt(req.query.offset as string)
+    const limit = queryLimit <= maxLimit ? queryLimit : maxLimit;
+    const offset = queryOffset < maxOffset ? queryOffset : 0;
+    return {
+        limit, offset
+    }
+}
