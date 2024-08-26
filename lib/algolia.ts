9f1c966049b05b01ce19104fec43f96a6d64e17f
adrianl01 <gustavo.adrian.leiva879@gmail.com>
2024-08-26 13:03:10 -0300

    fourth commit .env

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -1,6 +1,6 @@
 import algoliasearch from "algoliasearch"
 
-const client = algoliasearch("BTTD31H87B", "9f389d4b28114bdc0da0270648aa2597")
+const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ADMIN_API_KEY)
 const productIndex = client.initIndex("products")
 
 export { productIndex }
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -4,7 +4,22 @@ import { airtableBase } from "../../../lib/airtable";
 import { productIndex } from "../../../lib/algolia";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+    const { offset } = req.query;
+    const { limit } = req.query;
+    console.log("offset:", offset)
+    console.log("limit:", limit)
+    const strngOffset = JSON.parse(offset as string)
+    const strngLimit = JSON.parse(limit as string)
+    const results = await productIndex.search(req.query.search as string, { offset: strngOffset, length: strngLimit })
 
-    const results = await productIndex.search(req.query.search as string)
-    res.send(results)
+    res.send(
+        {
+            results,
+            pagination: {
+                offset: offset,
+                limit: limit,
+                total: results.nbHits
+            }
+        }
+    )
 }
